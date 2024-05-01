301217783bdef1cd5a5925769611773dfc54e279
labibahmed10 <labib.ahmed.372@gmail.com>
2024-05-02 01:27:03 +0600

    added header and flatlist on home page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
@@ -6,8 +6,19 @@ import { StatusBar } from "expo-status-bar";
 export default function Home() {
   return (
     <SafeAreaView>
-      <StatusBar style="light" backgroundColor="#1E1E2D" />
-      <Text>Home</Text>
+      <FlatList
+        data={[{ key: "a" }, { key: "b" }, { key: "c" }]}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
+        keyExtractor={(item) => item.key}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <Text>Header</Text>
+              <Text>Header</Text>
+            </View>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 }
